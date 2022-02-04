c6c2ae6181d6530eee77f3a506068e92cc476d60
Aloofed <74889534+Aloofed@users.noreply.github.com>
2022-02-04 19:28:53 +0800

    Add files via upload

---
+++ FinaleVM.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using Finale.Models;
+
+#nullable disable
+
+namespace Finale.VM
+{
+    public class FinaleVM
+    {
+        public List<Book> Books { get; set; }
+        public List<Author> Authors { get; set; }
+    }
+
+
+}
